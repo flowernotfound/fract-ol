f50ec1e180138057fb1d0545a81982202af60d04
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-19 15:11:58 +0900

    added features to delete the window (press ESC)

--- inc/fract-ol.h
+++ inc/fract-ol.h
@@ -4,11 +4,21 @@
 # include "mlx.h"
 # include <stdlib.h>
 
+// for debug
+# include <stdio.h>
+
 # define WINDOW_SIZE 600
+// キーコードはどのライブラリ使うかによって変わる
+// 今はminilibx-linuxを使ってるからこうなる　mac用のものでコンパイルすると個々変える必要ある
+# define KEY_ESC 65307
+# define X 120
 
 typedef struct s_data {
     void    *mlx;
     void    *win;
 } t_data;
+
+int key_hook(int keycode, t_data *data);
+int close_window(t_data *data);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,23 @@
 #include "../inc/fract-ol.h"
 
+int key_hook(int keycode, t_data *data)
+{
+	printf("keycode: %d\n", keycode);
+	if (keycode == KEY_ESC)
+	{
+		mlx_destroy_window(data->mlx, data->win);
+		exit(0);
+	}
+	return (0);
+}
+
+int close_window(t_data *data)
+{
+	mlx_destroy_window(data->mlx, data->win);
+	exit(0);
+	return (0);
+}
+
 int main(void)
 {
 	t_data data;
@@ -10,11 +28,21 @@ int main(void)
 		return (1);
 
 	// 引数を処理する
+
 	// ウィンドウを作成する
 	data.win = mlx_new_window(data.mlx, WINDOW_SIZE, WINDOW_SIZE, "first window");
+	if (data.win == NULL)
+	{
+		free(data.mlx);
+		return (1);
+	}
+
+	mlx_key_hook(data.win, key_hook, &data);
+	mlx_hook(data.win, X, 0, close_window, &data);
+
 	// イベントまち
 	mlx_loop(data.mlx);
-	// ウィンドウを閉じる
+
 	// mlxを閉じる
 
 	return (0);
