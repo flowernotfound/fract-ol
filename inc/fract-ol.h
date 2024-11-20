45b3fa2b70bcb9a19144a5c664767f093bb613d7
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-20 16:33:56 +0900

    イメージを作る部分を追加　一旦直線を書いた（テストで）

--- inc/fract-ol.h
+++ inc/fract-ol.h
@@ -13,12 +13,22 @@
 # define KEY_ESC 65307
 # define X 120
 
+typedef struct s_img {
+    void    *img;
+    char    *addr;
+    int     bits_per_pixel;
+    int     line_length;
+    int     endian;
+} t_img;
+
 typedef struct s_data {
     void    *mlx;
     void    *win;
+	t_img   img;
 } t_data;
 
 int key_hook(int keycode, t_data *data);
 int close_window(t_data *data);
+void    my_mlx_pixel_put(t_img *img, int x, int y, int color);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -21,6 +21,8 @@ int close_window(t_data *data)
 int main(void)
 {
 	t_data data;
+	int x;
+	int y;
 
 	// mlxを初期化する
 	data.mlx = mlx_init();
@@ -36,6 +38,29 @@ int main(void)
 		free(data.mlx);
 		return (1);
 	}
+
+	// img作る
+	// 新しいイメージの作成
+    data.img.img = mlx_new_image(data.mlx, WINDOW_SIZE, WINDOW_SIZE);
+	data.img.addr = mlx_get_data_addr(data.img.img,
+                                 &data.img.bits_per_pixel,
+                                 &data.img.line_length,
+                                 &data.img.endian);
+
+	y = 0;
+    while (y < WINDOW_SIZE)
+    {
+        x = 0;
+        while (x < WINDOW_SIZE)
+        {
+            if (x == y)
+                my_mlx_pixel_put(&data.img, x, y, 0x00FF0000);  // 赤色
+            x++;
+        }
+        y++;
+    }
+    // 作成したイメージをウィンドウに表示
+    mlx_put_image_to_window(data.mlx, data.win, data.img.img, 0, 0);
 
 	mlx_key_hook(data.win, key_hook, &data);
 	mlx_hook(data.win, X, 0, close_window, &data);
