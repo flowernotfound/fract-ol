cf402fa519df2f7ac42bad5e8e49783dfcfab831
Hiroaki Mitsuyoshi <thnksfrth3244@gmail.com>
2024-11-21 14:49:42 +0900

    header

--- src/utils.c
+++ src/utils.c
@@ -1,24 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hiroaki <hiroaki@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/21 14:48:24 by hiroaki           #+#    #+#             */
+/*   Updated: 2024/11/21 14:48:26 by hiroaki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../inc/fract-ol.h"
 
 void	clean_exit(t_data *data)
 {
-    if (data->img.img)
-        mlx_destroy_image(data->mlx, data->img.img);
-    if (data->win)
-        mlx_destroy_window(data->mlx, data->win);
-    #ifdef __linux__
-        if (data->mlx)
-            mlx_destroy_display(data->mlx);
-    #endif
-    if (data->mlx)
-        free(data->mlx);
-    exit(0);
+	if (data->img.img)
+		mlx_destroy_image(data->mlx, data->img.img);
+	if (data->win)
+		mlx_destroy_window(data->mlx, data->win);
+	if (data->mlx)
+		mlx_destroy_display(data->mlx);
+	if (data->mlx)
+		free(data->mlx);
+	exit(0);
 }
 
-void    my_mlx_pixel_put(t_img *img, int x, int y, int color)
+void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 {
-    char    *dst;
+	char	*dst;
 
-    dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
-    *(unsigned int*)dst = color;
+	dst = img->addr + (y * img->line_length + x * (img->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
 }
